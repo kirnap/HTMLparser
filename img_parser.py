7462684af4e6353ee08da5595dedb5f2f6a88897
OmerKirnap <okirnap@ku.edu.tr>
2015-12-11 21:36:17 +0200

    bug fix in outputstring

--- img_parser.py
+++ img_parser.py
@@ -1,8 +1,10 @@
-import copy
+# -​*- coding: utf-8 -*​-
+
+
+trial2 = """<img alt="toronto skyline" src="http://www.blogto.com/upload/2013/04/20130401-MB-Skyline.jpg" width="710"/><p>Metrolinx is  in the GTA. In a report due later this year, Metrolinx will select a few of these options to pitch to the provincial government. Premier Kathleen Wynne says Toronto will adopt at least one of the fees in order to pay for the much-needed Downtown Relief Line, among others.</p><p><a href="http://news.nationalpost.com/2013/04/01/metrolinx-to-unveil-25-money-making-ideas-for-funding-new-transit-infrastructure/">due to unveil the list of revenue tools it believes will fund the next phase of infrastructure projects</a></p><a href="http://news.nationalpost.com/2013/04/01/metrolinx-to-unveil-25-money-making-ideas-for-funding-new-transit-infrastructure/">due to unveil the list of revenue tools it believes will fund the next phase of infrastructure projects</a><p>Speaking of the Big Move, the Toronto Region Board of Trade, which recently released its own list of recommended transit taxes and tolls, has also <a href="http://www.thestar.com/news/gta/2013/04/01/transit_funding_toronto_region_board_of_trade_ad_campaign_aims_to_build_support_to_get_traffic_moving_again.html">started a radio and print ad campaign</a> to drum up support for its ideas. Do you think public opinion of transit taxes is shifting?</p><a href="http://www.thestar.com/news/gta/2013/04/01/transit_funding_toronto_region_board_of_trade_ad_campaign_aims_to_build_support_to_get_traffic_moving_again.html">started a radio and print ad campaign</a><p>Michael Nguyen, <a href="http://www.thestar.com/news/crime/2013/04/01/yorkdale_mall_shooting_victim_convicted_in_2005_windsor_home_invasion_toronto_gang_ties.html">the 23-year-old killed outside the Yorkdale Mall on the weekend</a>, had a lengthy criminal past and alleged gang ties, according to the <em>Toronto Star</em>. Nguyen was convicted over an armed home invasion in Windsor in 2005. Police also think the Alexandra Park resident was involved with the Asian Assassinz gang, a group responsible for petty vandalism and robbery.</p><a href="http://www.thestar.com/news/crime/2013/04/01/yorkdale_mall_shooting_victim_convicted_in_2005_windsor_home_invasion_toronto_gang_ties.html">the 23-year-old killed outside the Yorkdale Mall on the weekend</a><em>Toronto Star</em><p>Riding the subway or driving on the Gardiner, it's easy to overlook nature in Toronto. Right now, <a href="http://www.thestar.com/news/gta/2013/04/01/jumping_fish_offer_a_spectacle_of_nature_in_the_humber_river.html">rainbow trout are jumping up the Humber River at the Old Mill dam</a>. Sadly, according to the <em>Toronto Star</em>, the fish that do make it up stream are threatened by pollution.</p><a href="http://www.thestar.com/news/gta/2013/04/01/jumping_fish_offer_a_spectacle_of_nature_in_the_humber_river.html">rainbow trout are jumping up the Humber River at the Old Mill dam</a><em>Toronto Star</em><p>A <a href="http://www.thestar.com/life/food_wine/2013/04/01/toronto_soup_master_dies_suddenly.html">sad day for Toronto's food scene yesterday</a>. Ravi Kanagarajah, the "soup king" behind the three renowned RaviSoups locations in the city, died suddenly of a stroke on Wednesday, his family has announced. Kanagarajah arrived in Toronto after being forced to flee Sri Lanka in 1987. He was 42.</p><a href="http://www.thestar.com/life/food_wine/2013/04/01/toronto_soup_master_dies_suddenly.html">sad day for Toronto's food scene yesterday</a><p>Finally, the <a href="http://www.thestar.com/entertainment/books/2013/04/01/toronto_bookstore_nicholas_hoare_closes_for_good_on_monday.html">Nicholas Hoare bookstore is no more</a>. The Front St. institution closed its doors for the final time at the end of business yesterday, citing concerns about the publishing industry. Stores in Ottawa and Montreal have also closed in recent years. Hoare opened the store, which specialized in British books, in 1989. He's retiring to Halifax.</p><a href="http://www.thestar.com/entertainment/books/2013/04/01/toronto_bookstore_nicholas_hoare_closes_for_good_on_monday.html">Nicholas Hoare bookstore is no more</a><p><strong>CONTEST</strong></p><strong>CONTEST</strong><img alt="The Place Beyond the Pines" src="http://www.blogto.com/upload/2013/03/2013326-place-pines.jpg" width="710"/><p>The Place Beyond the Pines was the hottest ticket at TIFF 2012 so we're excited to announce that we've teamed up with eOne Films to give you a chance to be at an exclusive advance screening  of the film for blogTO readers only! This riveting new movie from the director of Blue Valentine is a sweeping emotional drama, powerfully exploring the unbreakable bond between fathers and sons.</p><p><strong><a href="http://contests.blogto.com/the-place-beyond-the-pines/">View all the contest details here</a></strong> for a chance to win passes to the screening as well as prizes courtesy of TIFF, the Trump Toronto and STOCK Restaurant.</p><strong><a href="http://contests.blogto.com/the-place-beyond-the-pines/">View all the contest details here</a></strong><a href="http://contests.blogto.com/the-place-beyond-the-pines/">View all the contest details here</a><p><iframe allowfullscreen="" frameborder="0" height="330" src="http://www.youtube.com/embed/gvsPRD3Eadc?list=PL1F7402FCD3DDF8B1" width="590"></iframe></p><iframe allowfullscreen="" frameborder="0" height="330" src="http://www.youtube.com/embed/gvsPRD3Eadc?list=PL1F7402FCD3DDF8B1" width="590"></iframe><p><strong>IN BRIEF:</strong></p><strong>IN BRIEF:</strong><p></p><ul><li><a href="http://www.thestar.com/news/gta/2013/04/01/firefighters_called_to_townhouse_blaze_near_york_university.html">Firefighters called to townhouse blaze near York University</a> [Toronto Star]</li><li><a href="http://www.thestar.com/news/crime/2013/04/01/police_release_name_of_couple_found_dead_in_their_home.html">Son charged with murder of couple found dead in their home</a> [Toronto Star]</li><li><a href="http://www.cbc.ca/news/canada/toronto/story/2013/03/31/toronto-double-homicide.html">Man, 26, accused of killing parents appears in court</a> [CBC]</li></ul><li><a href="http://www.thestar.com/news/gta/2013/04/01/firefighters_called_to_townhouse_blaze_near_york_university.html">Firefighters called to townhouse blaze near York University</a> [Toronto Star]</li><a href="http://www.thestar.com/news/gta/2013/04/01/firefighters_called_to_townhouse_blaze_near_york_university.html">Firefighters called to townhouse blaze near York University</a><li><a href="http://www.thestar.com/news/crime/2013/04/01/police_release_name_of_couple_found_dead_in_their_home.html">Son charged with murder of couple found dead in their home</a> [Toronto Star]</li><a href="http://www.thestar.com/news/crime/2013/04/01/police_release_name_of_couple_found_dead_in_their_home.html">Son charged with murder of couple found dead in their home</a><li><a href="http://www.cbc.ca/news/canada/toronto/story/2013/03/31/toronto-double-homicide.html">Man, 26, accused of killing parents appears in court</a> [CBC]</li><a href="http://www.cbc.ca/news/canada/toronto/story/2013/03/31/toronto-double-homicide.html">Man, 26, accused of killing parents appears in court</a><p><em>Chris Bateman is a staff writer at blogTO. Follow him on Twitter at <a href="https://twitter.com/chrisbateman">@chrisbateman</a>.</em></p><em>Chris Bateman is a staff writer at blogTO. Follow him on Twitter at <a href="https://twitter.com/chrisbateman">@chrisbateman</a>.</em><a href="https://twitter.com/chrisbateman">@chrisbateman</a><p><em>Image: <a href="http://www.flickr.com/photos/evan-russell/8611939942/in/pool-blogto">"On Top Of Things"</a> by Acid_Punk/<a href="http://www.flickr.com/groups/blogto/pool/">blogTO Flickr pool</a>.</em></p><em>Image: <a href="http://www.flickr.com/photos/evan-russell/8611939942/in/pool-blogto">"On Top Of Things"</a> by Acid_Punk/<a href="http://www.flickr.com/groups/blogto/pool/">blogTO Flickr pool</a>.</em><a href="http://www.flickr.com/photos/evan-russell/8611939942/in/pool-blogto">"On Top Of Things"</a><a href="http://www.flickr.com/groups/blogto/pool/">blogTO Flickr pool</a>"""
+trial = """ <p> <img alt="toronto skyline" src="http://www.blogto.com/upload/2013/04/20130401-MB-Skyline.jpg" width="710"/> Metrolinx is  in the GTA. In a report due later this year, Metrolinx will select a few of these options to pitch to the provincial government. Premier Kathleen Wynne says Toronto will adopt at least one of the fees in order to pay for the much-needed Downtown Relief Line, among others.   <a href="http://news.nationalpost.com/2013/04/01/metrolinx-to-unveil-25-money-making-ideas-for-funding-new-transit-infrastructure/"> due to unveil the list of revenue tools it believes will fund the next phase of infrastructure projects </a> </p> <p> Speaking of the Big Move, the Toronto Region Board of Trade, which recently released its own list of recommended transit taxes and tolls, has also <a href="http://www.thestar.com/news/gta/2013/04/01/transit_funding_toronto_region_board_of_trade_ad_campaign_aims_to_build_support_to_get_traffic_moving_again.html"> started a radio and print ad campaign </a> to drum up support for its ideas. Do you think public opinion of transit taxes is shifting? </p> <p> Michael Nguyen, <a href="http://www.thestar.com/news/crime/2013/04/01/yorkdale_mall_shooting_victim_convicted_in_2005_windsor_home_invasion_toronto_gang_ties.html"> the 23-year-old killed outside the Yorkdale Mall on the weekend </a> , had a lengthy criminal past and alleged gang ties, according to the <em> Toronto Star </em> . Nguyen was convicted over an armed home invasion in Windsor in 2005. Police also think the Alexandra Park resident was involved with the Asian Assassinz gang, a group responsible for petty vandalism and robbery. </p> <p> Riding the subway or driving on the Gardiner, it's easy to overlook nature in Toronto. Right now, <a href="http://www.thestar.com/news/gta/2013/04/01/jumping_fish_offer_a_spectacle_of_nature_in_the_humber_river.html"> rainbow trout are jumping up the Humber River at the Old Mill dam </a> . Sadly, according to the , the fish that do make it up stream are threatened by pollution. </p> <p> A <a href="http://www.thestar.com/life/food_wine/2013/04/01/toronto_soup_master_dies_suddenly.html"> sad day for Toronto's food scene yesterday </a> . Ravi Kanagarajah, the "soup king" behind the three renowned RaviSoups locations in the city, died suddenly of a stroke on Wednesday, his family has announced. Kanagarajah arrived in Toronto after being forced to flee Sri Lanka in 1987. He was 42. </p>"""
 
 import bs4 as bs
-from bs4 import NavigableString
-
 
 def read_from_string(input_string):
     """
@@ -20,7 +22,7 @@ def open_html_document_in_beautifulsoup(input_doc):
     :param input_doc:
     :return: BeautifulSoup instance
     """
-    return BeautifulSoup(open(input_doc), 'html5lib')
+    return bs.BeautifulSoup(open(input_doc), 'lxml')
 
 
 def find_container_p_tags(document):
@@ -36,26 +38,52 @@ def find_container_p_tags(document):
     return ret
 
 
-def beautify_content(doc, p_tag):
+def create_new_p(doc, p_contents):
     """
-    First insert all the elements after the current p_tags in order and then destroy the current tag
-    :param doc:
-    :param p_tag:
-    :return: None
+
+
     """
-    p_tag_contents = p_tag.contents
-    current = p_tag
-    for i in range(len(p_tag_contents)):
-        item = copy.copy(p_tag_contents[i])
-        if isinstance(item, NavigableString):
-            new_element = doc.new_tag('p')
-            new_element.string = item
-            current.insert_after(new_element)
-            current = current.next_sibling
+    new_p = doc.new_tag('p')
+    control_list = range(len(p_contents))
+    for i in control_list:
+        new_p.append(p_contents[0])
+    return new_p
+
+def parse_p(doc,p_tag, current=None):
+    if len(p_tag.contents) == 0:
+        p_tag.decompose()
+    else:
+        if not current:
+            current = p_tag
+        index = get_img_index(p_tag)
+        if index == 0:
+            img_tag = p_tag.contents[index]
+            current.insert_after(img_tag)
+            current = img_tag
+            parse_p(doc, p_tag, current=current)
+        elif isinstance(index, int) and index != 0:
+            img_tag = p_tag.contents[index]
+            new_p = create_new_p(doc, p_tag.contents[:index])
+            current.insert_after(new_p)
+            new_p.insert_after(img_tag)
+            current = img_tag
+            parse_p(doc, p_tag, current=current)
         else:
-            current.insert_after(item)
-            current = current.next_sibling
-    p_tag.decompose()
+            new_p = create_new_p(doc, p_tag.contents)
+            current.insert_after(new_p)
+            current = new_p
+            parse_p(doc, p_tag, current=current)
+
+
+
+def get_img_index(p_tag):
+    for i in range(len(p_tag.contents)):
+        if p_tag.contents[i].name == 'img':
+            return i
+    return None
+
+
+
 
 
 def find_img_tags(document):
@@ -88,6 +116,9 @@ def normalize_img_tags(img_list):
 
 
 def has_img_tag(tag):
+    """
+    Returns boolean to help find_container_p_tags function
+    """
     ret = False
     for child_tag in tag.findChildren():
         if child_tag.name == 'img':
@@ -96,20 +127,19 @@ def has_img_tag(tag):
 
 
 if __name__ == '__main__':
-    input_document = raw_input('Please enter html content as a string: ')
-    html_doc = read_from_string(input_document)
-    # html_doc = open_html_document_in_beautifulsoup(input_document)
-    img_tags = find_img_tags(html_doc)
-    normalize_img_tags(img_tags)
-    for tag in find_container_p_tags(html_doc):
-        beautify_content(html_doc, tag)
-
-
+    doc = read_from_string(trial)
+    p = find_container_p_tags(doc)
+    for tag in find_container_p_tags(doc):
+        parse_p(doc,tag)
     result = open('result.html', 'w')
-    for item in html_doc.body.findChildren():
-        result.write(str(item))
 
-    result.close()
+    result.write(str(doc.body.findChildren()[0]))
+    txt = str(doc.body.findChildren()[0])
+    for item in doc.body.findChildren()[0].find_next_siblings():
+        txt += (str(item))
+    print txt
+
+
 
 
 
